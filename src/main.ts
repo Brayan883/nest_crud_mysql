6d31f104485c235e024b5ba5fcb7b5512d544de3
Brayan Rivera Gomez <100776460+Brayan883@users.noreply.github.com>
2024-06-20 18:59:39 -0500

    add cors

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  app.setGlobalPrefix('api/v1');
+
+  const whitelist = ["http://localhost:3000", "http://localhost:3001"];
+
+  app.enableCors({
+    credentials: true,
+    origin: function (origin, callback) {
+      if (!whitelist.includes(origin) || !origin) {
+        callback(new Error("Not allowed by CORS"));
+      } else {
+        callback(null, true);
+      }
+    },
+  });
+
+  app.setGlobalPrefix("api/v1");
 
   app.useGlobalPipes(
     new ValidationPipe({
